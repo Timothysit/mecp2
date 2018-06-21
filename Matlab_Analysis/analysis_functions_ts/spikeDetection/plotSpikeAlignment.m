6035d13351833497eba214510c09ef5e6f7fa7bb
Timothy sit <sitpakhang@gmail.com>
2018-06-21 10:15:46 +0100

    Added ghost method

--- Matlab_Analysis/analysis_functions_ts/spikeDetection/plotSpikeAlignment.m
+++ Matlab_Analysis/analysis_functions_ts/spikeDetection/plotSpikeAlignment.m
@@ -1,13 +1,20 @@
 function plotSpikeAlignment(spikeMatrix, method, fs, durationInSec)
 
 % Author: Tim Sit, sitpakhang@gmail.com
-% Last update: 20180405
+% Last update: 20180621
 % 20180405: Added positive / negative peaks
+% 20180621: Clairifed that I added the "ghost method" (for the organoid project)
 
 % fs = 25000; 
 % durationInSec = 2 * 10^-3; 
 durationInFrame = fs * durationInSec;
 
+% INPUT 
+    % method
+        % 'simple' makes individual as different colours
+        % the 'ghost' method makes the individual spikes as gray
+        % semitransparent traces 
+    
 
 % if ~exist(method) 
 %     method = 'simple';
@@ -35,6 +42,15 @@ end
 %     plot(spikeMatrix(i, :))
 %     hold on
 % end 
+
+% ghost method 
+% makes each trace semitransparent gray traces 
+if strcmp(method, 'ghost') 
+    xAxis = [0:size(spikeMatrix, 2)-1] - ((size(spikeMatrix, 2) - 1) /2);
+    alpha = 0.3; % 0 for black, 1 for white
+    plot(xAxis, spikeMatrix', 'Color', [0 0 0] + alpha) 
+end
+
 
 
 % a slightly more sophisticated method based on finding the peak and
@@ -98,6 +114,42 @@ if strcmp(method, 'peak3D')
     xlim([1 length(spStart:spEnd)]);
     grid; 
     view(40,40);
+end 
+
+
+if strcmp(method, 'peakghost')
+    % fprintf('Using peak method \n') 
+    alpha = 0.2;
+    for spikeTimeSeries = 1:size(spikeMatrix, 1)
+        % find positive peaks
+        % [pks,locs] = findpeaks(spikeMatrix(spikeTimeSeries, :));
+        % spikePeakLoc = locs(pks == max(pks)); % the spike peak is the max peak 
+        % that's an assumption, but should be true in most cases
+        % find negative peaks 
+        [pks,locs] = findpeaks(-spikeMatrix(spikeTimeSeries, :));
+        spikePeakLoc = locs(abs(pks) == max(abs(pks)));
+        
+        spStart = spikePeakLoc - round(durationInFrame / 2);
+        spEnd = spikePeakLoc + round(durationInFrame / 2);
+        if spStart > 0 && spEnd < size(spikeMatrix, 2)
+            plot(spikeMatrix(spikeTimeSeries, spStart:spEnd), 'Color', [0 0 0] + 1 - alpha);
+        else
+            warning(['Spike exceeded limit, not plotted. Spike Number: ' num2str(spikeTimeSeries)])
+        end 
+        hold on
+    end 
+    % plot the average waveform on top 
+    aveSpikeWaveForm = mean(spikeMatrix);
+    [pks,locs] = findpeaks(-aveSpikeWaveForm);
+    spikePeakLoc = locs(abs(pks) == max(abs(pks)));
+    spStart = spikePeakLoc - round(durationInFrame / 2);
+    spEnd = spikePeakLoc + round(durationInFrame / 2);
+    if spStart > 0 && spEnd < size(spikeMatrix, 2)
+       plot(aveSpikeWaveForm(spStart:spEnd), 'Color', [0 0 0]);
+    else
+       warning(['Spike exceeded limit, not plotted. Spike Number: ' num2str(spikeTimeSeries)])
+    end 
+    
 end 
 
 
