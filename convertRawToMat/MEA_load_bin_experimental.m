69bea4d44f55ad696926a158612bd0b32597a652
Timothy sit <sitpakhang@gmail.com>
2018-06-26 15:23:41 +0100

    Update MEA_load_bin_experimental.m

--- convertRawToMat/MEA_load_bin_experimental.m
+++ convertRawToMat/MEA_load_bin_experimental.m
@@ -1,72 +1,173 @@
-function MEA_batchConvert(ext)
-%form: MEA_batchConvert
-%This function converts MEA bin (binary, RAW) files into mat files in a batch. It will
-%attempt to convert all "raw" files in the directory, so make sure they are
-%all MEA files or enter an extension to select a few.
+function [header,m,channels] = MEA_load_bin_experimental(binfile,plt)
+% THIS IS A EXPERIMENTAL FORK 
+% There is some change this will not work (or worse, output wrong values)
+% if the recording duration is not exactly 720 seconds
+% This function truly reads the .raw file one channel at a time, which aims
+% to reduce the amount of memory required
+
+% note that I (TS) is going to modify this slightly to make it work with
+% any number of channels 
+% also changed the code so that it saves one giant variable 
+% rather than create a subfolder 
+% also makes running combineMat.m unncessary 
+
+%form: 
+%This function takes txt file output from MC_Datatool (Multichannel
+%Systems) and converts it into a matlab matrix.
 %
-%To create the RAW file, 
-%1. open MC_DataTool
-%2. File-Open Multiple
-%3. Select files of interest
-%4. Click "bin"
-%5. Click "All"
-%6. Make sure "Write header" and "Signed 16bit" are checked in lower right
-%7. Click Save
-%8. when done, click Close
 
-% Modified by Tim Sit from nictera at: 
-% https://github.com/nictera/Matlab_MEA
+% Instructions 
 
-% TODO: Improve the command line output of this
+% Do not run this directly, this is called by MEAbatchConvert.m 
+% But a lot of the key steps of the conversion processes are contained here
+% The main thing to change is 'convertOption', which you should specify in
+% MEAbatchConvert.m, the default is to do 'whole'; the entire grid will be
+% saved in one variable 
+% the other option is to do electrode by electrode conversion, in which
+% case convertOption == 'electrode'
 
 % Last update: 20180626 
-% TS: Added the conversion options
 
-%% Select conversion mode 
+% Log
+% 20180626: Added option to save each electrode as an individual .mat file 
+% (to avoid memory issues on systems with less than 16GB ram / low swap space)
 
-convertOption = 'electrode'; % save electrode by electrode in a MEA-specific folder
-% convertOption = 'whole'; % save the entire grid as one variable
 
 %% initialize
 
-if ~exist('ext','var')
-    ext='.raw';
+sprintf('MEA_load_bin')
+
+if ~exist('plt','var')
+    plt=0;
 end;
 
-ext2='.raw';
+if ~exist('convertOption', 'var')
+    convertOption = 'whole'; 
+end 
 
-%% get files
 
-d=dir;
-files=[];
 
-for i=1:length(d)
-    if ~isempty(findstr(d(i).name,ext)) && ~isempty(findstr(d(i).name,ext2)) && length(d(i).name)>2 
-        files=[files; i];
-    end;
+%% get header
+[fid] =fopen(binfile)
+[filename, permission, machineformat, encoding] =fopen(fid);
+[m,count]=fread(fid,2000,'*char',0,machineformat);
+fclose(fid);
+
+%% get key values from header
+
+m=m';
+f=findstr(m,'EOH');
+newstart=f+4;
+header=m(1:f-1)
+
+ADCzerof=findstr(m,'ADC');
+ADCz = str2num(m(ADCzerof+11:ADCzerof+16));
+
+fsf=findstr(m,'Sample rate');
+fs=str2num(m(fsf+14:fsf+18));
+
+uVf=findstr(m,'�V/AD');
+uV = str2num(m(uVf-7:uVf-1));
+
+
+%% get channels from header
+
+chf=findstr(header,'Streams');
+chs = header(chf+11:length(header))
+channels=[];
+while length(chs)>5
+    f=findstr(chs,'_');
+    channels=[channels; str2num(chs(f(1)+1:f(1)+2))];
+    chs(1:f(1)+2)=[];
 end;
+channels;
+size(channels)
 
-files=d(files);
+%% get data size and from that, **estimate** the duration of recording
 
-%% convert the files
+% currently, the estimation works for 720 seconds, but I am not sure if it
+% will work for other recording duration, so this is the section that may
+% break the code 
+[fid] =fopen(binfile);
+dataLength = get_file_length(fid);
+fclose(fid);
 
-for i=1:length(files)
-    files(i).name
-    skip=0;
-    %find if file already converted
-    for j=1:length(d)
-        if ~isempty(strmatch(files(i).name(1:length(files(i).name)-4),d(j).name)) && d(j).isdir
-            d(j).name
-            skip=1;
-        end;
-    end;
-    if skip==0
-       %  MEA_load_bin(files(i).name, 0, convertOption);
-       MEA_load_bin_experimental(files(i).name);
-    end;
-end;
+recordDuration = dataLength / length(channels);
+% I have no idea why I have to divide by 2... but somehow it works
+% probably something to do with the coding of binary values... but I am not
+% quite sure...
+recordDuration = round(recordDuration / 2, -1); % round to nearest 10^1
 
 
+%% get data
+mkdir(binfile(1:length(binfile)-4))
+% start looping through channels here
+for i=1:length(channels)
+% this (aims to) read the binary file channel by channel
+
+[fid] =fopen(binfile);
+[trash]=fread(fid,newstart,'*char',0,machineformat);
+[data]=fread(fid,recordDuration,'int16',0,machineformat);%change to uint16 for data from older versions of MC_tool
+fclose(fid);
+
+% directly assign channel to m, no need to reshape 
+m = data;
+
+%% reshape data
+
+%each row is a different channel
+% numChannels = length(channels); 
+% m=reshape(data,numChannels,floor(length(data)/numChannels));
+% note that I (TS) chaged '60' to numChannels 
+% not sure if there will be any consequences... 
+% need to check through the logic of this code to make sure
+
+
+%% convert data to microvolts (see MC_datatool help - search 'binary')
+max(m(:));
+min(m(:));
+% 
+m=m-ADCz;
+m=m*uV;
+dat = m'; % matlab reads tall matrices more efficiently
+% tall means number of rows > number of columns
+%% plot
+
+% if plt
+%     ch=82;
+%     figure
+%     t=1:size(m,2);
+%     t=t/fs;
+%     size(m)
+%     f=find(channels==ch)
+%     plot(t,m(f(1),:),'k')
+% end;
+
+%% save
+% sprintf('Saving data ...')
+
+% /mkdir(binfile(1:length(binfile)-4))
+% the crux to not having folders is here 
+% save([binfile(1:length(binfile)-4) filesep binfile(1:length(binfile)-4) '.mat'],'dat','channels','header','uV','ADCz','fs')
+% the above line saves to specific subfolders 
+
+% OPTION 1: Save all electrodes as a single file (about 2GB variable), only a good idea if your system has 16GB ram / a lot of swap space
+% if strcmp(convertOption, 'whole')
+%     save([binfile(1:length(binfile)-4) '.mat'],'dat','channels','header','uV','ADCz','fs', '-v7.3')
+% % OPTION 2: For systems with 8GB ram (or less?), save each electrode in a separate file 
+% elseif strcmp(convertOption, 'electrode')
+%     mkdir(binfile(1:length(binfile)-4))
+%     for i=1:length(channels)
+%          sprintf('Channel: %d; ',channels(i))
+%          dat=m(i,:);
+%          save([binfile(1:length(binfile)-4) filesep binfile(1:length(binfile)-4) '_' num2str(channels(i)) '.mat'],'dat','channels','header','uV','ADCz','fs')
+%     end 
+%     
+% end 
+dat = m; 
+sprintf('Channel: %d; ',channels(i))
+save([binfile(1:length(binfile)-4) filesep binfile(1:length(binfile)-4) '_' num2str(channels(i)) '.mat'],'dat','channels','header','uV','ADCz','fs')
+end % end looping of all channels 
 
 end
 
