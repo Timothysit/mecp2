611e5a73fba10f81fad795f6f81961dc6bd3cb4b
timsit <sitpakhang@gmail.com>
2020-06-25 14:50:20 +0100

    dealt with rare case of multiple maximam in the spike

--- Matlab_Analysis/analysis_functions_ts/spikeDetection/plotSpikeAlignment.m
+++ Matlab_Analysis/analysis_functions_ts/spikeDetection/plotSpikeAlignment.m
@@ -4,16 +4,22 @@ function plotSpikeAlignment(spikeMatrix, method, fs, durationInSec)
 % Last update: 20180621
 % 20180405: Added positive / negative peaks
 % 20180621: Clairifed that I added the "ghost method" (for the organoid project)
+% 20200628: Default behaviour now plots time relative to peak on the x-axis
 
 % fs = 25000; 
 % durationInSec = 2 * 10^-3; 
 durationInFrame = fs * durationInSec;
+x_axis_units = 'ms-rel-to-peak';  
 
-% INPUT 
+% Parameters
     % method
         % 'simple' makes individual as different colours
         % the 'ghost' method makes the individual spikes as gray
         % semitransparent traces 
+    % x_axis_units (str)
+        % what to plot on the x-axis
+        % if 'ms-rel-to-peak' this will be the time in milliseconds 
+        % relative to the peak of spike
     
 
 % if ~exist(method) 
@@ -31,7 +37,11 @@ durationInFrame = fs * durationInSec;
 if strcmp(method, 'simple')
     % this makes sures that 0 is the point where the spike is detected
     % only tested when number of frames is an odd number (63)
-    xAxis = [0:size(spikeMatrix, 2)-1] - ((size(spikeMatrix, 2) - 1) /2);
+    if strcmp(x_axis_units, 'ms-rel-to-peak')
+        xAxis = linspace(-durationInSec/2, durationInSec/2, size(spikeMatrix, 2));
+    else
+        xAxis = [0:size(spikeMatrix, 2)-1] - ((size(spikeMatrix, 2) - 1) /2);
+    end 
     plot(xAxis, spikeMatrix') 
     % title('Simple method') 
     % aesthetics
@@ -46,7 +56,11 @@ end
 % ghost method 
 % makes each trace semitransparent gray traces 
 if strcmp(method, 'ghost') 
-    xAxis = [0:size(spikeMatrix, 2)-1] - ((size(spikeMatrix, 2) - 1) /2);
+    if strcmp(x_axis_units, 'ms-rel-to-peak')
+        xAxis = linspace(-durationInSec/2, durationInSec/2, size(spikeMatrix, 2));
+    else
+        xAxis = [0:size(spikeMatrix, 2)-1] - ((size(spikeMatrix, 2) - 1) /2);
+    end 
     alpha = 0.3; % 0 for black, 1 for white
     plot(xAxis, spikeMatrix', 'Color', [0 0 0] + alpha) 
 end
@@ -60,6 +74,12 @@ end
 % figure 
 if strcmp(method, 'peak')
     % fprintf('Using peak method \n') 
+    if strcmp(x_axis_units, 'ms-rel-to-peak')
+        xAxis = linspace(-durationInSec/2, durationInSec/2, size(spikeMatrix, 2));
+    else
+        xAxis = [0:size(spikeMatrix, 2)-1] - ((size(spikeMatrix, 2) - 1) /2);
+    end 
+    
     for spikeTimeSeries = 1:size(spikeMatrix, 1)
         % find positive peaks
         % [pks,locs] = findpeaks(spikeMatrix(spikeTimeSeries, :));
@@ -72,7 +92,7 @@ if strcmp(method, 'peak')
         spStart = spikePeakLoc - round(durationInFrame / 2);
         spEnd = spikePeakLoc + round(durationInFrame / 2);
         if spStart > 0 && spEnd < size(spikeMatrix, 2)
-            plot(spikeMatrix(spikeTimeSeries, spStart:spEnd));
+            plot(xAxis, spikeMatrix(spikeTimeSeries, spStart:spEnd));
         else
             warning(['Spike exceeded limit, not plotted. Spike Number: ' num2str(spikeTimeSeries)])
         end 
@@ -85,6 +105,14 @@ end
 % 3D Plot so overlapping lines are more obvious 
 if strcmp(method, 'peak3D')
     plotSpikeCount = 0; 
+    
+    % Define time axis 
+    if strcmp(x_axis_units, 'ms-rel-to-peak')
+        xAxis = linspace(-durationInSec/2, durationInSec/2, size(spikeMatrix, 2)) * 1000; 
+    else
+        xAxis = [0:size(spikeMatrix, 2)-1] - ((size(spikeMatrix, 2) - 1) /2);
+    end 
+    
     % fprintf('Using peak method \n') 
     for spikeTimeSeries = 1:size(spikeMatrix, 1)
         % find positive peaks
@@ -119,6 +147,8 @@ end
 
 if strcmp(method, 'peakghost')
     % fprintf('Using peak method \n') 
+    
+
     alpha = 0.2;
     peakAlignedSpikeMatrix  = [];
     for spikeTimeSeries = 1:size(spikeMatrix, 1)
@@ -130,10 +160,24 @@ if strcmp(method, 'peakghost')
         [pks,locs] = findpeaks(-spikeMatrix(spikeTimeSeries, :));
         spikePeakLoc = locs(abs(pks) == max(abs(pks)));
         
+        if length(spikePeakLoc) > 1
+            spikePeakLoc = spikePeakLoc(1);
+        end 
+        
         spStart = spikePeakLoc - round(durationInFrame / 2);
         spEnd = spikePeakLoc + round(durationInFrame / 2);
+        
+         % Define time axis 
+        if strcmp(x_axis_units, 'ms-rel-to-peak')
+
+            xAxis = linspace(-durationInSec/2, durationInSec/2, spEnd - spStart + 1) * 1000; 
+
+        else
+            xAxis = spStart:spEnd;
+        end 
+  
         if spStart > 0 && spEnd < size(spikeMatrix, 2)
-            plot(spikeMatrix(spikeTimeSeries, spStart:spEnd), 'Color', [0 0 0] + 1 - alpha);
+            plot(xAxis, spikeMatrix(spikeTimeSeries, spStart:spEnd), 'Color', [0 0 0] + 1 - alpha);
             peakAlignedSpikeMatrix = [peakAlignedSpikeMatrix; ...
                 spikeMatrix(spikeTimeSeries, spStart:spEnd)];
                 
@@ -153,7 +197,7 @@ if strcmp(method, 'peakghost')
     spStart = spikePeakLoc - round(durationInFrame / 2);
     spEnd = spikePeakLoc + round(durationInFrame / 2);
     if spStart > 0 && spEnd < size(spikeMatrix, 2)
-       plot(aveSpikeWaveForm(spStart:spEnd), 'Color', [0 0 0]);
+       plot(xAxis, aveSpikeWaveForm(spStart:spEnd), 'Color', [0 0 0]);
     else
        warning(['Spike exceeded limit, not plotted. Spike Number: ' num2str(spikeTimeSeries)])
     end 
