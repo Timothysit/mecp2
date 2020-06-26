c70040612439f6f87a734db91e1f2d4187eebeba
timsit <sitpakhang@gmail.com>
2020-06-26 16:06:49 +0100

    added option to group intersection by rounding to arbitrary values

--- Matlab_Analysis/analysis_functions_ts/spikeDetection/findGroupIntersectSpikes.m
+++ Matlab_Analysis/analysis_functions_ts/spikeDetection/findGroupIntersectSpikes.m
@@ -1,4 +1,4 @@
-function [intersection_matrix, unique_spike_times] = findGroupIntersectSpikes(spike_struct, fs, round_decimal_places)
+function [intersection_matrix, unique_spike_times] = findGroupIntersectSpikes(spike_struct, fs, round_decimal_places, round_mode)
 %FINDGROUPINTERSECTSPIKES Find matching spikes from multiple methods.
 % Obtains the unique spike times from a structure containing the detected
 % spike using multiple methods, and outputs a matrix where each row 
@@ -6,6 +6,11 @@ function [intersection_matrix, unique_spike_times] = findGroupIntersectSpikes(sp
 % represents a spike detection method. An entry of 1 in the matrix 
 % means that the spike detection method identified a spike at that time 
 % and 0 otherwise. 
+
+% About the variabels
+% spike_times : vector of spike times in seconds
+
+supported_round_modes = {'decimal_places', 'nearest_to_value'};
 
 
 wavelet_method_used = fieldnames(spike_struct);
@@ -23,7 +28,12 @@ end
 % allow rounding to the nearest 0.02 ms 
 % eg. see: https://uk.mathworks.com/matlabcentral/answers/9641-how-do-i-round-to-the-nearest-arbitrary-enumerated-value-in-matlab
 if round_decimal_places > 0
-    all_spike_times = round(all_spike_times, round_decimal_places);
+    if strcmp(round_mode, 'decimal_places')
+        all_spike_times = round(all_spike_times, round_decimal_places);
+    elseif  strcmp(round_mode, 'nearest_to_value')
+        round_targets = min(all_spike_times):round_decimal_places:max(all_spike_times);
+        all_spike_times = interp1(round_targets, round_targets, all_spike_times, 'nearest');
+    end 
 end 
 
 
@@ -38,7 +48,13 @@ for method_number = 1:numel(wavelet_method_used)
     spike_times = spike_struct.(wavelet_method_used{method_number}) / fs;
     
     if round_decimal_places > 0
-        spike_times = round(spike_times, round_decimal_places);
+        if strcmp(round_mode, 'decimal_places')
+            spike_times = round(spike_times, round_decimal_places);
+        elseif strcmp(round_mode, 'nearest_to_value')
+            round_targets = min(all_spike_times):round_decimal_places:max(all_spike_times);
+            spike_times = interp1(round_targets, round_targets, spike_times, 'nearest');
+            % spike_times = floor(spike_times * 1/round_decimal_places) / (1 / round_decimal_places);
+        end
     end 
     
     for unq_spk_t_index = 1:length(unique_spike_times)
