c7d059b7c090a09abcad9166b4cae40f42366c02
timsit <sitpakhang@gmail.com>
2020-09-12 12:51:51 +0100

    added loadmat

--- python/mea_util.py
+++ python/mea_util.py
@@ -2,6 +2,70 @@ import xarray as xr
 import h5py
 import numpy as np
 
+import scipy.io as spio
+
+def loadmat(filename, struct_as_record=False):
+    """
+    A better loadmat function than the default scipy one
+    This function should be called instead of direct spio.loadmat
+    as it cures the problem of not properly recovering python dictionaries
+    from mat files. It calls the function check keys to cure all entries
+    which are still mat-objects
+    Parameters
+    -----------
+    struct_as_record : if True, you access fields using data['field']
+    if False, you access field using dot notation: data.field
+
+    """
+
+    def _check_keys(d):
+        '''
+        checks if entries in dictionary are mat-objects. If yes
+        todict is called to change them to nested dictionaries
+        '''
+        for key in d:
+            if isinstance(d[key], spio.matlab.mio5_params.mat_struct):
+                d[key] = _todict(d[key])
+        return d
+
+    def _todict(matobj):
+        '''
+        A recursive function which constructs from matobjects nested dictionaries
+        '''
+        d = {}
+        for strg in matobj._fieldnames:
+            elem = matobj.__dict__[strg]
+            if isinstance(elem, spio.matlab.mio5_params.mat_struct):
+                d[strg] = _todict(elem)
+            elif isinstance(elem, np.ndarray):
+                # TS: Make sure numpy array is at least 1 dimensional
+                # Otherwise the loop in _tolist() will complain about 0-d arrays.
+                elem = np.atleast_1d(elem)
+                d[strg] = _tolist(elem)
+            else:
+                d[strg] = elem
+        return d
+
+    def _tolist(ndarray):
+        '''
+        A recursive function which constructs lists from cellarrays
+        (which are loaded as numpy ndarrays), recursing into the elements
+        if they contain matobjects.
+        '''
+        elem_list = []
+        for sub_elem in ndarray:
+            if isinstance(sub_elem, spio.matlab.mio5_params.mat_struct):
+                elem_list.append(_todict(sub_elem))
+            elif isinstance(sub_elem, np.ndarray):
+                elem_list.append(_tolist(sub_elem))
+            else:
+                elem_list.append(sub_elem)
+        return elem_list
+
+    data = spio.loadmat(filename, struct_as_record=struct_as_record, squeeze_me=True)
+    return _check_keys(data)
+
+
 
 def hdf_loadmat(mat_filepath):
     f = h5py.File(mat_filepath)
