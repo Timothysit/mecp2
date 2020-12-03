6243608ec0b6129194e13f43f03e704274a288b1
timsit <sitpakhang@gmail.com>
2020-12-03 14:37:16 +0000

    accepts recordin sampling frequency if spike times are in frames

--- Matlab_Analysis/analysis_functions_ts/spikeTimeToMatrix.m
+++ Matlab_Analysis/analysis_functions_ts/spikeTimeToMatrix.m
@@ -1,6 +1,14 @@
-function spike_matrix = spikeTimeToMatrix(spikeTimesStruct, start_time, end_time, sampling_rate);
+function spike_matrix = spikeTimeToMatrix(spikeTimesStruct, start_time, ...
+    end_time, sampling_rate, recording_fs)
 
+% recording_fs (optional) : (int)
+% If this argument is provided, then spikeTimes are in frames rather than
+% in seconds, and the spike time in second is obtained by dividing
+% spikeFrames by recordign_fs
 
+if ~exist('recording_fs', 'var')
+    recording_fs = 0;
+end 
 
 bin_edges = start_time:1/sampling_rate:end_time;
 num_bins = length(bin_edges) - 1; 
@@ -12,6 +20,11 @@ if isstruct(spikeTimesStruct)
     spike_matrix = zeros(num_bins, num_channels);
     for channel_idx = 1:numel(channel_names)
         channel_spike_times = spikeTimesStruct.(channel_names{channel_idx});
+       
+        if recording_fs ~= 0
+            channel_spike_times = channel_spike_times / recording_fs;
+        end 
+        
         spike_vector = histcounts(channel_spike_times, bin_edges);
         spike_matrix(:, channel_idx) = spike_vector;
     end 
@@ -23,6 +36,11 @@ elseif iscell(spikeTimesStruct)
     spike_matrix = zeros(num_bins, num_channels);
     for channel_num = 1:length(spikeTimesCell)
         channel_spike_times = spikeTimesCell{channel_num}.('mea');
+        
+        if recording_fs ~= 0
+            channel_spike_times = channel_spike_times / recording_fs;
+        end 
+        
         spike_vector = histcounts(channel_spike_times, bin_edges);
         spike_matrix(:, channel_num) = spike_vector;
     end 
